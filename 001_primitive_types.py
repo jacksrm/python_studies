a52400d4b52b959b7946d85de602187bee6182d7
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-07-28 06:31:16 -0300

    Adds lesson 01

---
+++ 001_primitive_types.py
@@ -0,0 +1,16 @@
+# Basic primitive types:
+# - int -> Integer
+# - float -> Float Point numbers
+# - bool -> Boolean True and False
+# - str -> String
+
+# n1 = input("Type a value: ")
+# print(type(n1)) // <class 'str'>
+
+# n1 = int(input("Type a value: "))
+# print(type(n1)) // <class 'str'>
+
+print("Insert 2 numbers: ")
+n1 = int(input("N1: "))
+n2 = int(input("N2: "))
+print(f"Sum: {n1} + {n2} = {n1 + n2}")
