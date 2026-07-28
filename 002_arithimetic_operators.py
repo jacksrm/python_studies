16f09310719b633014f83e8106d9b1b9a784ff95
Jacson Rodrigues <github.obsessive123@passmail.net>
2026-07-28 07:58:27 -0300

    Adds lesson 02

---
+++ 002_arithimetic_operators.py
@@ -0,0 +1,27 @@
+# Arithmetic Operators
+# -> +  Sum
+# -> -  Subtraction
+# -> *  Multiplication
+# -> /  Division
+# -> ** Power
+# -> // Integer Division
+# -> %  Module
+n1 = 5
+n2 = 2
+
+print(f"             Sum: {n1} + {n2} = {n1 + n2}")
+print(f"     Subtraction: {n1} - {n2} = {n1 - n2}")
+print(f"  Multiplication: {n1} * {n2} = {n1 * n2}")
+print(f"        Division: {n1} / {n2} = {n1 / n2}")
+print(f"           Power: {n1} ** {n2} = {n1**n2}")
+print(f"Integer Division: {n1} // {n2} = {n1 // n2}")
+print(f"          Module: {n1} % {n2} = {n1 % n2}")
+
+# Precedence Order
+# 1. ()
+# 2. **
+# 3. * / // %
+# 4. + -
+# If theres more than one operator of the same precedence
+# from left -> to right
+# first -> second
